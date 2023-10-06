e917ba1dc169f4b1f68d0d345fd3dc2b54ae5c1a
HajMar <hajaniaina.raharison@outlook.com>
2023-10-06 05:58:34 +0300

    home_gui

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,27 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_livre_btn_clicked()
+{
+    ui->principal->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_membre_btn_clicked()
+{
+    ui->principal->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_emprunt_btn_clicked()
+{
+    ui->principal->setCurrentIndex(3);
+}
+
+
+void MainWindow::on_lb_accueil_clicked()
+{
+    ui->principal->setCurrentIndex(0);
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_livre_btn_clicked();
+
+    void on_membre_btn_clicked();
+
+    void on_emprunt_btn_clicked();
+
+    void on_lb_accueil_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
